aa76ab48f330927bddfd6f55c39a20e145792e49
hailthedawn <kkt7778@rit.edu>
2022-04-26 15:06:42 -0400

    f1, f2, f3, f6, f7 cost functions added

--- p1_function.py
+++ p1_function.py
@@ -3,6 +3,60 @@ import sys
 from numpy import random
 import math
 
+# Ackley function
+def F_01(x):
+    exp_term2  = 0
+    exp_term1 = 0
+    for item in x:
+        for i in range(0, len(item)-1):
+            exp_term1 = exp_term1 + math.pow(item[i], 2)
+            exp_term2 = exp_term2 + math.cos(2*math.pi*item[i])
+    exp_term1 = math.exp(-0.2 * math.sqrt(exp_term1 * 0.5))
+    exp_term2 = math.exp(0.5 * exp_term2)
+    return -20.0 * exp_term1 - exp_term2 + math.e + 20
+
+
+# Fletcher-Powell function
+def F_02(x):
+    Ai = 0
+    for item in x:
+        for i in range:
+            pass
+        pass
+    return 0
+
+
+# Griewank Function
+def F_03(x):
+    term_1 = 0
+    term_2 = 1
+    for item in x:
+        for i in range(0, len(item)):
+            term_1 = term_1 + item[i]**2
+            term_2 = term_2 * math.cos(item[i]/math.sqrt(i+1))
+            
+    term_1 = term_1/4000
+    return term_1 - term_2 + 1
+
+
+# Quartic function with noise 
+def F_06(x):
+    sum_term = 0
+    for item in x:
+        for i in range(0, len(item)):
+            sum_term = sum_term + (i+1)*math.pow(item[i],4) + np.random.uniform(0, 1)
+    return sum_term
+
+
+# Rastrigin Function
+def F_07(x):
+    sum_term = 0
+    for item in x:
+        for i in range(0, len(item)):
+            sum_term = sum_term + item[i]**2 - 10*math.cos(2*math.pi*item[i])
+    return 10*len(item) + sum_term
+
+
 
 # Rosenbrock function 
 def F_08(x): 
